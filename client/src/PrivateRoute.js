52328eb8c98f2cb40984dfe67e78ff1d6bceff8e
Marina Harrison <marinaharrison1@gmail.com>
2021-06-07 17:46:29 -0700

    comments

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { Consumer } from './Context';
 
+//This component make private routes only for authenticated users to use for update, create, and delete course
 export default ({ component: Component, ...rest }) => {
   return (
     <Consumer>
